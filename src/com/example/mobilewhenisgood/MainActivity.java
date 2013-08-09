9e95063c5b5c59a638a0c1b7958daf766769a331
Garrett <emrickgarrett@gmail.com>
2013-08-09 09:50:18 -0400

    Changes to click listener
    Changes

--- src/com/example/mobilewhenisgood/MainActivity.java
+++ src/com/example/mobilewhenisgood/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.mobilewhenisgood;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
+	
+	public static String TAG = "WhenIsGood";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView create = (TextView) findViewById(R.id.create_event);
+		TextView browse = (TextView) findViewById(R.id.browse_events);
+		create.setOnClickListener(this);
+		browse.setOnClickListener(this);
 	}
 
 	@Override
@@ -21,16 +29,18 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
-	/**
-	 * Used for the text views in the project
-	 */
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch(v.getId()){
-		
+		case R.id.create_event:
+			System.out.println("Event created");
+			break;
+		case R.id.browse_events:
+			System.out.println("Browse Events");
+			break;
 		}
-		
 	}
 
 }
