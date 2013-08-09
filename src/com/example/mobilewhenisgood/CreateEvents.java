ec60240f6fa2804d28e1095072bab65f225a9cb7
Garrett <emrickgarrett@gmail.com>
2013-08-09 11:14:06 -0400

    Edits to create events
    Could use some formatting to make it look better

--- src/com/example/mobilewhenisgood/CreateEvents.java
+++ src/com/example/mobilewhenisgood/CreateEvents.java
@@ -1,10 +1,13 @@
 package com.example.mobilewhenisgood;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class CreateEvents extends Activity {
+public class CreateEvents extends Activity implements OnClickListener {
 
 	public UserInfo dummy;
 	
@@ -22,6 +25,28 @@ public class CreateEvents extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_events, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		View area = findViewById(R.id.create_content_area);
+		
+		switch(v.getId()){
+		case R.id.date_field:
+			System.out.println("date field");
+			break;
+		case R.id.time_field:
+			System.out.println("Time Field");
+			break;
+		case R.id.end_date_field:
+			System.out.println("End Date Field");
+			break;
+		case R.id.create_event_button:
+			System.out.println("Event Created!");
+			break;
+		}
+		
 	}
 
 }
