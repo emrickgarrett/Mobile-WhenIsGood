3475f5abef754d7cce35e4e673d92033e6eb6024
Garrett <emrickgarrett@gmail.com>
2013-08-09 09:35:36 -0400

    Small Adjustments
    Strings and XML

--- src/com/example/mobilewhenisgood/MainActivity.java
+++ src/com/example/mobilewhenisgood/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.mobilewhenisgood;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/**
+	 * Used for the text views in the project
+	 */
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		
+		}
+		
 	}
 
 }
