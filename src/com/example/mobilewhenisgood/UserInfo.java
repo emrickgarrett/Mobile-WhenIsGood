bda55323000e318e1af3977471c24be8887cbeda
Garrett <emrickgarrett@gmail.com>
2013-08-09 09:21:52 -0400

    Base Classes
    Some base classes and descriptions

---
+++ src/com/example/mobilewhenisgood/UserInfo.java
@@ -0,0 +1,16 @@
+package com.example.mobilewhenisgood;
+
+/**
+ * 
+ * @author Garrett
+ * Dummy class to simulate the user already being logged in.
+ */
+public class UserInfo {
+	
+	private String userName = "GarrettEmrick";
+	private String password = "abc123";
+	
+	public String getUserName(){return userName;}
+	public String getPassword(){return password;}
+	
+}
